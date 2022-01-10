e253d4f6b907bf3fcac1c34390eae31805737497
Javier F <javierfalconc@gmail.com>
2022-01-10 06:38:35 -0800

    Add missing lambda dependency to make Lo4j2 work.
    Add a missing dependency and add a log to the default route.

--- src/main/java/org/falcocloud/lambdatemplate/routing/routes/DefaultRoute.java
+++ src/main/java/org/falcocloud/lambdatemplate/routing/routes/DefaultRoute.java
@@ -1,6 +1,7 @@
 package org.falcocloud.lambdatemplate.routing.routes;
 
 import lombok.experimental.SuperBuilder;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.falcocloud.lambdatemplate.model.lambda.APIGatewayProxyRequest;
 import org.falcocloud.lambdatemplate.model.service.ServiceResponse;
@@ -8,6 +9,7 @@ import org.falcocloud.lambdatemplate.model.service.ServiceResponse;
 import java.util.Map;
 
 @SuperBuilder
+@Log4j2
 public class DefaultRoute extends Route {
   public static final String RESOURCE = "/invalidpath/default";
   public static final String HTTP_METHOD = "DEFAULT";
@@ -15,7 +17,8 @@ public class DefaultRoute extends Route {
       ImmutablePair.of(RESOURCE, HTTP_METHOD);
 
   @Override
-  public ServiceResponse process(APIGatewayProxyRequest request) {
+  public ServiceResponse process(final APIGatewayProxyRequest request) {
+    log.warn("Invalid request", request);
     return ServiceResponse.builder()
         .statusCode(404)
         .headers(request.getHeaders())
