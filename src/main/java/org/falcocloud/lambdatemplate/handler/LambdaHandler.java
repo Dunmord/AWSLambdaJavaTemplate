bf75c5afd8c0a0b7b7dd3154dbcf4ff824b91b19
Javier F <javierfalconc@gmail.com>
2022-02-18 07:57:42 -0800

    General improvements
    Add a constructor to the LambdaHandler to be able to add mocks through
    unit testing, add support for post filters to the routing agent and
    initialize the routing agent when the lambda first gets initialized.

--- src/main/java/org/falcocloud/lambdatemplate/handler/LambdaHandler.java
+++ src/main/java/org/falcocloud/lambdatemplate/handler/LambdaHandler.java
@@ -9,6 +9,7 @@ import org.falcocloud.lambdatemplate.model.lambda.APIGatewayProxyRequest;
 import org.falcocloud.lambdatemplate.model.lambda.APIGatewayProxyResponse;
 import org.falcocloud.lambdatemplate.routing.RoutingAgent;
 import org.falcocloud.lambdatemplate.routing.routes.DefaultRoute;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -18,24 +19,35 @@ public class LambdaHandler
     implements RequestHandler<APIGatewayProxyRequest, APIGatewayProxyResponse> {
 
   private final Gson gson;
+  private final RoutingAgent routingAgent;
 
   public LambdaHandler() {
     // Initialize the SDK client outside of the handler method so that it can be reused for
     // subsequent invocations.
     // It is initialized when the class is loaded.
     gson = DependencyFactory.gsonInstance();
+    routingAgent =
+        RoutingAgent.builder()
+            .preFilters(getAllPreFilters())
+            .routes(Map.of(DefaultRoute.RESOURCE_HTTP_METHOD_PAIR, DefaultRoute.builder().build()))
+            .build();
     // Consider invoking a simple api here to pre-warm up the application, eg: dynamodb#listTables
+  }
+
+  // For easy unit testing
+  public LambdaHandler(final DynamoDbClient dynamoDbClient) {
+    gson = DependencyFactory.gsonInstance();
+    routingAgent =
+        RoutingAgent.builder()
+            .preFilters(getAllPreFilters())
+            .routes(Map.of(DefaultRoute.RESOURCE_HTTP_METHOD_PAIR, DefaultRoute.builder().build()))
+            .build();
   }
 
   @Override
   public APIGatewayProxyResponse handleRequest(
       final APIGatewayProxyRequest request, Context context) {
-    var response =
-        RoutingAgent.builder()
-            .preFilters(getAllPreFilters())
-            .routes(Map.of(DefaultRoute.RESOURCE_HTTP_METHOD_PAIR, DefaultRoute.builder().build()))
-            .build()
-            .process(request);
+    var response = routingAgent.process(request);
     return APIGatewayProxyResponse.builder()
         .statusCode(response.getStatusCode())
         .headers(response.getHeaders())
@@ -44,6 +56,10 @@ public class LambdaHandler
   }
 
   private FilterQueue getAllPreFilters() {
+    return FilterQueue.builder().filters(new LinkedList<>(List.of())).build();
+  }
+
+  private FilterQueue getAllPostFilters() {
     return FilterQueue.builder().filters(new LinkedList<>(List.of())).build();
   }
 }
--- src/main/java/org/falcocloud/lambdatemplate/routing/RoutingAgent.java
+++ src/main/java/org/falcocloud/lambdatemplate/routing/RoutingAgent.java
@@ -2,6 +2,7 @@ package org.falcocloud.lambdatemplate.routing;
 
 import lombok.Builder;
 import lombok.NonNull;
+import lombok.val;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.falcocloud.lambdatemplate.filters.FilterException;
@@ -18,6 +19,7 @@ public class RoutingAgent {
 
   private final Map<Pair<String, String>, Route> routes;
   @NonNull private FilterQueue preFilters;
+  private FilterQueue postFilters;
 
   public ServiceResponse process(final APIGatewayProxyRequest request) {
     try {
@@ -32,10 +34,24 @@ public class RoutingAgent {
                   "An error has occurred. Please contact the system administrator."))
           .build();
     }
-    return routes
-        .getOrDefault(
-            ImmutablePair.of(request.getResource(), request.getHttpMethod()),
-            routes.get(DefaultRoute.RESOURCE_HTTP_METHOD_PAIR))
-        .process(request);
+    val response =
+        routes
+            .getOrDefault(
+                ImmutablePair.of(request.getResource(), request.getHttpMethod()),
+                routes.get(DefaultRoute.RESOURCE_HTTP_METHOD_PAIR))
+            .process(request);
+    try {
+      postFilters.doFilter(request);
+    } catch (final FilterException e) {
+      return ServiceResponse.builder()
+          .statusCode(500)
+          .headers(Map.of())
+          .body(
+              Map.of(
+                  ServiceResponse.HTTP_RESPONSE_KEY,
+                  "An error has occurred. Please contact the system administrator."))
+          .build();
+    }
+    return response;
   }
 }
